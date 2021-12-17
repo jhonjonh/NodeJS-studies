f4891fa8f104567181c47429eaad94014353fbce
jhonjonh <john.gerber@live.com>
2021-12-17 19:27:56 -0300

    criada var para porta

--- http/app.js
+++ http/app.js
@@ -1,7 +1,8 @@
 var http = require("http");
+const porta = 8181;
 
 http.createServer(function(requisicao, resposta){
     resposta.end("<h1>Bem vindo ao meu site!</h1><h4>Guia do Pogramador</h4>");
-}).listen(8181);
+}).listen(porta);
 
-console.log("Meu servidor está rodando");
+console.log("Servidor rodando com sucesso na porta" + porta);
