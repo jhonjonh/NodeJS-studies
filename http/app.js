82d14d636fa794afb6e85648799ec9565193171b
jhonjonh <john.gerber@live.com>
2021-12-17 19:28:38 -0300

    ajuste var porta console

--- http/app.js
+++ http/app.js
@@ -5,4 +5,4 @@ http.createServer(function(requisicao, resposta){
     resposta.end("<h1>Bem vindo ao meu site!</h1><h4>Guia do Pogramador</h4>");
 }).listen(porta);
 
-console.log("Servidor rodando com sucesso na porta" + porta);
+console.log("Servidor rodando com sucesso na porta " + porta);
